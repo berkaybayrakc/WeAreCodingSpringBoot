e731d96730130d01537bc4aca87a58c661469c83
Muhammet Berkay Bayrakci <berkay7554@gmail.com>
2022-10-02 13:41:18 +0300

    first commit ( WeAreCodingSpringBoot)

---
+++ src/main/java/com/berkaybayrakci/WeAreCodingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.berkaybayrakci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeAreCodingSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeAreCodingSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/berkaybayrakci/WeAreCodingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkaybayrakci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeAreCodingSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
