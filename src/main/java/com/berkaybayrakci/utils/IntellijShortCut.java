68118173320fab661f387d467769cc9e2bd833f5
Muhammet Berkay Bayrakci <berkay7554@gmail.com>
2022-10-03 14:54:01 +0300

    IntellijShortCut

---
+++ src/main/java/com/berkaybayrakci/utils/IntellijShortCut.java
@@ -0,0 +1,10 @@
+package com.berkaybayrakci.utils;
+
+public class IntellijShortCut {
+//    Setting  > ctrl + alt + s
+//    Generate > Alt + Insert
+//    single comment > ctrl + /
+//    multiple comment > ctrl + shift + /
+
+
+}
