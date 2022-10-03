1e194c08efc806e903b76209e4e2e5fff76be05f
Muhammet Berkay Bayrakci <berkay7554@gmail.com>
2022-10-03 14:53:10 +0300

    SpringBootApplication ===> Security (exclude)

--- src/main/java/com/berkaybayrakci/WeAreCodingSpringBootApplication.java
+++ src/main/java/com/berkaybayrakci/WeAreCodingSpringBootApplication.java
@@ -3,7 +3,13 @@ package com.berkaybayrakci;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//Security inactive
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+       org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
+
+//@SpringBootApplication
 public class WeAreCodingSpringBootApplication {
 
     public static void main(String[] args) {
