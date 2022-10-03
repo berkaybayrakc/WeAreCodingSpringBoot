0f683e09cdaf07eff3b5cd9ef99afd9bd632bfe7
Muhammet Berkay Bayrakci <berkay7554@gmail.com>
2022-10-03 14:55:32 +0300

    DevTools Activate

---
+++ src/main/java/com/berkaybayrakci/utils/ConfigProgramming.java
@@ -0,0 +1,13 @@
+package com.berkaybayrakci.utils;
+
+public class ConfigProgramming {
+
+//     DevTools > LiveReload
+//     file > settings > compiler > Build project automatically (OK clicked) (ctrl + alt + s)
+
+//     file > Advanced Settings > Allow Auto-make to start even (OK clicked)
+
+
+
+
+}
