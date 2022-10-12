3b3ec6388d067df4d4e27046dbad971c912bc2e6
Muhammet Berkay Bayrakci <berkay7554@gmail.com>
2022-10-12 03:53:06 +0300

    EmployeeServices (interface)

---
+++ src/main/java/com/berkaybayrakci/business/services/EmployeeServices.java
@@ -0,0 +1,24 @@
+package com.berkaybayrakci.business.services;
+
+import com.berkaybayrakci.business.dto.EmployeeDto;
+import com.berkaybayrakci.data.entity.EmployeeEntity;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
+
+public interface EmployeeServices {
+
+    //CRUD
+    public List<EmployeeDto> getAllEmployees();
+    public EmployeeDto createEmployee(EmployeeDto employeeDto);
+    public ResponseEntity<EmployeeDto> getEmployeeById(Long id) throws Throwable;
+    public ResponseEntity<EmployeeDto> updateEmployee(Long id,EmployeeDto employeeDto) throws Throwable;
+    public ResponseEntity<Map<String,Boolean>> deleteEmployee(Long id) throws Throwable;
+
+    // model Mapper
+    public EmployeeDto EntityToDto(EmployeeEntity employeeEntity);
+    public EmployeeEntity DtoToEntity(EmployeeDto employeeDto);
+
+
+}
